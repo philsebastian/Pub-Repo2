8e4dc2404f77244e96cf57e637df132936947e00
philsebastian <philsebastian@u.boisestate.edu>
2017-09-12 12:17:41 -0600

    MaxHeap.java implemented v1
    See comments

--- HeapNode.java
+++ HeapNode.java
@@ -1,38 +1,59 @@
 /**
+ * Class representing a node in a heap ADT. 
  * 
- */
-
-/**
- * @author Phillip Sebastian
+ * @author CS 321 
  *
+ * @param <T> - generic object of type T 
  */
-public class HeapNode<T>{
-
-	private T object;
-	private int key;
+public class HeapNode<T>
+{
+	private T object;	// reference to object stored at node
+	private int key;		// reference to key of object 
 	
-	public HeapNode(T data, int startKey) {
-		this.object = data;
-		this.key = startKey;
+	/**
+	 * Basic constructor  
+	 */
+	public HeapNode(T object, int key)
+	{
+		setObject(object); 
+		setKey(key); 
 	}
 	
-	public T getObject() {
-		// TODO 
-		return null;
+	/**
+	 * Returns object stored in node. 
+	 * @return - object of type T 
+	 */
+	public T getObject()
+	{
+		return object;
 	}
-
-	public void setObject(T data) {
-		// TODO 
-		
+	
+	/**
+	 * Sets object stored in node to given value. 
+	 * @param object - object of type T 
+	 */
+	public void setObject(T object)
+	{
+		this.object = object;
 	}
-
-	public int getKey() {
-		// TODO 
-		return 0;
+	
+	/**
+	 * Returns key value of the node
+	 * @return - int value 
+	 */
+	public int getKey()
+	{
+		return key;
 	}
-
-	public void setKey(int key) {
-		// TODO		
-	}
-
+	
+	/**
+	 * Sets key to given value 
+	 * @param key - int value 
+	 */
+	public void setKey(int key)
+	{
+		this.key = key;
+	} 
+	
+	
 }
--- MaxHeap.java
+++ MaxHeap.java
@@ -1,85 +1,155 @@
-
+import java.util.Arrays;
+/**
+ * 
+ * @author Phillip Sebastian
+ *
+ * @param <T>
+ */
 public class MaxHeap<T>{
 	
-	private HeapNode<T>[] heap;
+	private HeapNode<?>[] heap; // PHIL TODO - check this
 	private int heapSize;
 	private int capacity;
 	private int DEFAULT_CAPACITY = 11;
-	
+	/**
+	 * 
+	 */
 	public MaxHeap() {
-		this.capacity = 11;
-		this.heapSize = 0;
-		this.heap = new HeapNode[12];
+		this.setCapacity(this.DEFAULT_CAPACITY);
+		this.setHeapSize(0);
+		this.heap = new HeapNode<?>[this.capacity + 1]; // PHIL TODO - check this
 	}
-	
-	public MaxHeap(T[] dataArray, int[] keyArray) {
+	/**
+	 * 
+	 * @param dataArray
+	 * @param keyArray
+	 */
+	public MaxHeap(T[] dataArray, int[] keyArray) { 	
+		this.setCapacity(dataArray.length);
+		this.setHeapSize(dataArray.length);
+		this.heap = new HeapNode<?>[this.capacity + 1]; // PHIL TODO - check this
 		
+		// Below A.K.A. Build-Max-Heap
+		// First build array
+		for (int i = 0; i < dataArray.length; i++) {
+			this.heap[i + 1] = new HeapNode<T>(dataArray[i], keyArray[i]);
+		}
+		// Max-Heapify the Array
+		for (int i = this.heapSize / 2; i > 0; i--) {
+			this.maxHeapify(i);
+		}
 	}
-	
+	/**
+	 * 
+	 * @return
+	 */
 	public T heapMax() {
-		// TODO 
-		return null;
+		return (T) this.heap[1].getObject();
 	}
-
+	/**
+	 * 
+	 * @return
+	 */
 	public T extractHeapMax() {
-		// TODO 
-		return null;
+		T tmpT = this.heapMax();
+		this.heap[1] = this.heap[this.heapSize--];
+		this.maxHeapify(1);
+		return tmpT;
 	}
-
+	/**
+	 * 
+	 * @param location
+	 * @param key
+	 */
 	public void increaseHeapKey(int location, int key) {
-		// TODO 
-		
+		this.heap[location].setKey(key);
+		this.maxHeapify(this.parent(location));		
 	}
-
+	/**
+	 * 
+	 * @param data
+	 * @param key
+	 */
 	public void maxHeapInsert(T data, int key) {
-		// TODO 
-		
+		if (this.heapSize == this.capacity) {
+			this.expandCapacity();
+		}
+		this.heap[++this.heapSize] = new HeapNode<T>(data, key);
+		if (this.heapSize > 1) {
+			this.maxHeapify(this.parent(this.heapSize));		
+		}
 	}
-
+	/**
+	 * 
+	 * @param location
+	 */
 	public void maxHeapify(int location) {
-		// TODO 
-		
+		int largest = 0;
+		int left = this.left(location);
+		int right = this.right(location);		
+		if (left <= this.heapSize && this.heap[left].getKey() > this.heap[location].getKey()) {
+			largest = left;
+		} else {
+			largest = location;
+		}
+		if (right <= this.heapSize && this.heap[right].getKey() > this.heap[largest].getKey()) {
+			largest = right;
+		}
+		if (largest != location) {
+			this.exchange(location, largest);
+			this.maxHeapify(largest);
+		}		
 	}
-
+	/**
+	 * 
+	 * @return
+	 */
 	public int getHeapSize() {
 		return this.heapSize;
 	}
-
+	/**
+	 * 
+	 * @return
+	 */
 	public boolean isEmpty() {
 		return (this.heapSize == 0);
 	}
 	
 	private void expandCapacity() {
-		// TODO
+		this.setCapacity(this.heapSize * 2);
+		this.heap = Arrays.copyOf(this.heap, this.capacity);		
 	}
 	
+	// TODO - ask about this method - why and what it does?
 	private void moveUp(int location) {
 		// TODO
 	}
 	
 	private void exchange(int locationA, int locationB) {
-		// TODO
+		HeapNode<T> priorA = (HeapNode<T>) this.heap[locationA];
+		this.heap[locationA] = this.heap[locationB];
+		this.heap[locationB] = priorA;
+		priorA = null;
 	}
 	
 	private int left(int location) {
-		// TODO
-		return 0;
+		return (location * 2);
 	}
 	
 	private int right(int location) {
-		// TODO
-		return 0;
+		return (location * 2 + 1);
 	}
 	
 	private int parent(int location) {
-		// TODO
-		return 0;
+		return (location / 2);
 	}
 	
+	// TODO - Ask why this method
 	private void setHeapSize(int size) {
 		this.heapSize = size;
 	}
 	
+	// TODO - Ask why this method
 	private void setCapacity(int newCapacity) {
 		this.capacity = newCapacity;
 	}
